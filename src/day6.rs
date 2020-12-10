b38b1fbf733bfbe80ed2e3b4a594b572daf02fe2
jepaan <jepaan@users.noreply.github.com>
2020-12-10 20:17:22 +0100

    Day 6 part 2.

--- src/day6.rs
+++ src/day6.rs
@@ -7,7 +7,10 @@ pub fn printResult()
 {
 
     let mut sum = 0;
+    let mut sum2 = 0;
     let mut found: BTreeSet<char> = BTreeSet::new();
+    let mut found2: BTreeSet<char> = BTreeSet::new();
+    let mut first = true;
     if let Ok(lines) = fileUtil::readLines("data/input6.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
@@ -15,22 +18,42 @@ pub fn printResult()
             {
                 if value.len() > 0
                 {
+                    let mut thisLine: BTreeSet<char> = BTreeSet::new();
                     for c in value.chars()
                     {
+                        thisLine.insert(c);
                         found.insert(c);
-                    }
 
+                    }
+                    if first
+                    {
+                        found2 = thisLine.clone();
+                        first = false;
+                    }
+                    else
+                    {
+                        let d :Vec<char>= found2.difference(&thisLine).cloned().collect();
+                        for x in d
+                        {
+                            found2.remove(&x);
+                        }
+
+                    }
                 }
                 else
                 {
                     sum += found.len();
+                    sum2 += found2.len();
                     found.clear();
+                    found2.clear();
+                    first = true;
                 }
             }
         }
     }
     sum += found.len();
-    println!("The sum is {}", sum);
+    sum2 += found2.len();
+    println!("The sum is {} {}", sum, sum2);
 
 
 }
