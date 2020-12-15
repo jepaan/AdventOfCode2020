ab6e73449798e612058d9f223d583f7528f65720
jepaan <jepaan@users.noreply.github.com>
2020-12-15 22:52:48 +0100

    Day 9 part 2.

--- src/day9.rs
+++ src/day9.rs
@@ -1,12 +1,14 @@
 #![allow(non_snake_case)]
 
 pub use crate::fileUtil::fileUtil;
+use std::collections::VecDeque;
 
 pub fn printResult()
 {
     let size: usize = 25;
     let mut numbers: [i64; 25] = [0; 25];
     let mut i = 0;
+    let mut weakness= 0;
     if let Ok(lines) = fileUtil::readLines("data/input9.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
@@ -40,6 +42,7 @@ pub fn printResult()
                         if !foundMatch
                         {
                             println!("{}: {} is wrong!", i, number);
+                            weakness = number;
                             break;
                         }
                     }
@@ -48,6 +51,46 @@ pub fn printResult()
                 }
             }
         }
-
     }
+
+    let mut sequence: VecDeque<i64> = VecDeque::new();
+
+    fn compareAndTrim(list: & mut VecDeque<i64>, number: i64) -> bool
+    {
+        let mut sum : i64 = list.iter().sum();
+        //println!("Sum is {}", sum);
+        while sum > number
+        {
+            sum -= list.pop_front().unwrap();
+            //println!("Popped sum to {}", sum);
+        }
+        return sum == number;
+    }
+
+    if let Ok(lines) = fileUtil::readLines("data/input9.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(value) = line
+            {
+                if value.len() > 0
+                {
+                    let number: i64 = value.parse().unwrap();
+                    sequence.push_back(number);
+                    if compareAndTrim(&mut sequence, weakness)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    fn sumMinMax(list: &VecDeque<i64>) -> i64
+    {
+        return list.iter().min().unwrap() + list.iter().max().unwrap();
+    }
+
+    println!("The result is {}", sumMinMax(&sequence));
+
 }
+
