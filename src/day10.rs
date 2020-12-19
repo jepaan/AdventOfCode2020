890b2c0237169135295c3b222ad6b937d348a83a
jepaan <jepaan@users.noreply.github.com>
2020-12-19 20:54:53 +0100

    Day 10 part 2 but too slow.

--- src/day10.rs
+++ src/day10.rs
@@ -34,7 +34,53 @@ pub fn printResult()
             _ => (),
         }
     }
-    println!("{} {} {}", ones, threes, ones*threes);
+    println!("{} {} {}", ones, threes, ones * threes);
+
+    let finalValue = *numbers.last().unwrap();
+
+    //Make new vector and push in active routes
+    let mut routes: Vec<i32> = Vec::new();
+    routes.push(0);
+    while !routes.is_empty()
+    {
+        let value = routes.pop().unwrap();
+        //println!("{} value", value);
+        if value == finalValue
+        {
+            routes.push(value);
+            break;
+        }
+
+
+
+        let plus1=value+1;
+        let plus2 = value+2;
+        let plus3 = value+3;
+        if numbers.contains(&plus1)
+        {
+            //println!("{} can go to {}", value, plus1);
+            routes.push(plus1);
+        }
+        if numbers.contains(&plus2)
+        {
+            //println!("{} can go to {}", value, plus2);
+            routes.push(plus2);
+        }
+        if numbers.contains(&plus3)
+        {
+            //println!("{} can go to {}", value, plus3);
+            routes.push(plus3);
+        }
+        routes.sort_by(|a, b| b.cmp(a));
+        //for element in &routes
+        //{
+        //    print!("{} ", element);
+        //}
+        //print!("\n");
+
+    }
+    println!("Found {} ways!", routes.len());
+
 
 }
 
