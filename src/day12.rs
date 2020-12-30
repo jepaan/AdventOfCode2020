da5d2ab627ad6e56a742c41c1b738d1f601c76fd
jepaan <jepaan@users.noreply.github.com>
2020-12-30 22:32:53 +0100

    More restructure.

--- src/day12.rs
+++ src/day12.rs
@@ -9,16 +9,89 @@ use scan_fmt::scan_fmt;
 //2 west
 // south
 
+
+fn directionToValue(input: char) -> i32
+{
+    return match input
+    {
+        'E' => 0,
+        'N' => 1,
+        'W' => 2,
+        'S' => 3,
+        _ => panic!("Wrong input"),
+    }
+}
+
+trait Ferry
+{
+    fn forward(&mut self, distance: i64);
+    fn changeDirection(&mut self, direction: char, value: i64);
+    fn rotateLeft(&mut self, degrees: i64);
+    fn rotateRight(&mut self, degrees: i64);
+}
+
 struct Ferry1
 {
     location: (i64, i64),
     direction: i32,
+}
 
+impl Ferry for Ferry1
+{
+    fn forward(&mut self, distance: i64)
+    {
+        match self.direction
+        {
+            0 => self.location.0 += distance,
+            1 => self.location.1 += distance,
+            2 => self.location.0 -= distance,
+            3 => self.location.1 -= distance,
+            _ => panic!("Oh ne"),
+        }
+    }
+
+    fn changeDirection(&mut self, direction: char, value: i64)
+    {
+        let oldDirection = self.direction;
+        self.direction = directionToValue(direction);
+        self.forward(value);
+        self.direction = oldDirection;
+    }
+
+    fn rotateLeft(&mut self, mut degrees: i64)
+    {
+        while degrees > 0
+        {
+            self.direction += 1;
+            degrees -= 90;
+        }
+        self.direction  %= 4;
+    }
+
+    fn rotateRight(&mut self, mut degrees: i64)
+    {
+        while degrees > 0
+        {
+            if self.direction == 0
+            {
+                self.direction = 4;
+            }
+            self.direction -= 1;
+            degrees -= 90;
+        }
+    }
+}
+
+struct Ferry2
+{
+    location: (i64, i64),
+    waypoint: (i64, i64),
 }
 
 pub fn printResult()
 {
     let mut ferry1 = Ferry1 {location: (0, 0), direction: 0 };
+    let mut ferry2 = Ferry2 {location: (0, 0), waypoint: (10, 1) };
 
     if let Ok(lines) = fileUtil::readLines("data/input12.txt") {
         // Consumes the iterator, returns an (Optional) String
@@ -32,15 +105,17 @@ pub fn printResult()
                     let c = it.next().unwrap();
                     let n = it.collect::<String>().parse().unwrap();;
                     //println!("{} {}", c, n);
-                    ferry1.location = match c
+
+                    //Ferry 1
+                    match c
                     {
-                        'F' => moveFerry(ferry1.direction, ferry1.location, n),
-                        'N'  => moveFerry(1, ferry1.location, n),
-                        'W'  => moveFerry(2, ferry1.location, n),
-                        'S'  => moveFerry(3, ferry1.location, n),
-                        'E'  => moveFerry(0, ferry1.location, n),
-                        'L' => {ferry1.direction = rotateFerryLeft(ferry1.direction, n); ferry1.location},
-                        'R' => {ferry1.direction = rotateFerryRight(ferry1.direction, n); ferry1.location},
+                        'F' => ferry1.forward(n),
+                        'N'  => ferry1.changeDirection(c, n),
+                        'W'  => ferry1.changeDirection(c, n),
+                        'S'  => ferry1.changeDirection(c, n),
+                        'E'  => ferry1.changeDirection(c, n),
+                        'L' => ferry1.rotateLeft(n),
+                        'R' => ferry1.rotateRight(n),
 
                         _ => panic!("Unexpected"),
                     };
@@ -50,44 +125,6 @@ pub fn printResult()
         }
     }
 
-    fn moveFerry(direction: i32, location: (i64, i64), distance: i64) -> (i64, i64)
-    {
-        //println!("Direction {} distance {}", direction, distance);
-        return match direction
-        {
-            0 => (location.0 + distance, location.1),
-            1 => (location.0, location.1 + distance),
-            2 => (location.0 - distance, location.1),
-            3 => (location.0, location.1 - distance),
-            _ => panic!("Oh ne"),
-        }
-    }
-
-    fn rotateFerryLeft(mut direction: i32, mut degrees: i64) -> i32
-    {
-        //println!("Left {}",degrees);
-        while degrees > 0
-        {
-            direction += 1;
-            degrees -= 90;
-        }
-        return direction % 4;
-    }
-
-    fn rotateFerryRight(mut direction: i32, mut degrees: i64) -> i32
-    {
-        //println!("Right {}",degrees);
-        while degrees > 0
-        {
-            if direction == 0
-            {
-                direction = 4;
-            }
-            direction -= 1;
-            degrees -= 90;
-        }
-        return direction;
-    }
 
     println!("Manhatten distance is {}. Location {} {}", i64::abs(ferry1.location.0) + i64::abs(ferry1.location.1), ferry1.location.0, ferry1.location.1);
 }
