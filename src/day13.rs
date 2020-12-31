72f0573abfd8311c7a04adf9350ee4a6c765f498
jepaan <jepaan@users.noreply.github.com>
2020-12-31 12:07:21 +0100

    Day13 part 2 works but too slow for actual data.

--- src/day13.rs
+++ src/day13.rs
@@ -3,18 +3,27 @@
 
 pub use crate::fileUtil::fileUtil;
 
+struct Bus
+{
+    id: i64,
+    offset: i64
+}
+
+
+
 pub fn printResult()
 {
     if let Ok(lines) = fileUtil::readLines("data/input13.txt")
     {
         let mut iter = lines.into_iter();
         let time: i64 = iter.next().unwrap().unwrap().parse().unwrap();
-        println!("Time {}", time);
 
         let mut closest = i64::max_value();
         let mut id: i64 = 0;
         let secondLine = iter.next().unwrap().unwrap();
         let busses = secondLine.split(',');
+        let mut currentOffset = 0;
+        let mut busList : Vec<Bus> = Vec::new();
         for bus in busses
         {
             let this = String::from(bus).parse();
@@ -28,7 +37,6 @@ pub fn printResult()
                             //Perfect match
                             id = this;
                             closest = 0;
-                            break;
                         }
                         else
                         {
@@ -40,10 +48,36 @@ pub fn printResult()
                                 id = this;
                             }
                         }
+                        busList.push(Bus{ id: this, offset: currentOffset });
                     },
                 Err(this) => {},
             }
+            currentOffset += 1;
         }
         println!("Bus {} is next. Result is {}", id, id*closest);
+
+        let mut otherTimestamp:i64 = 100000000000000; //100000000000000
+        while otherTimestamp > 0
+        {
+            let mut isOk = true;
+            for bus in &busList
+            {
+                let t2 = otherTimestamp + bus.offset;
+                if t2 % bus.id != 0
+                {
+                    isOk = false;
+                    break;
+                }
+            }
+            if isOk
+            {
+                break;
+            }
+            else
+            {
+                otherTimestamp += 1;
+            }
+        }
+        println!("Other time is {}", otherTimestamp);
     }
 }
