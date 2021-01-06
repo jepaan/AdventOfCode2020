a5832fd7a1b179061c5aea1a0c35e00c9e72d63a
jepaan <jepaan@users.noreply.github.com>
2021-01-06 21:40:19 +0100

    Day 14 part 2.

--- src/day14.rs
+++ src/day14.rs
@@ -45,6 +45,71 @@ fn doPart1(line: &String, memory:& mut HashMap<usize, u64>, andMask: &mut u64, o
     }
 }
 
+fn flunctuate(indexes: &Vec<usize>, currentIndex: usize, values: &mut Vec<usize>, value: usize)
+{
+    //println!("Index {} value {}", currentIndex, value);
+    if currentIndex == indexes.len()
+    {
+        values.push(value)
+    }
+    else
+    {
+        let lowMask = usize::max_value() ^ (1 << indexes[currentIndex]);
+        let valueLow = value & lowMask;
+        let valueHigh = value | (1 << indexes[currentIndex]);
+        flunctuate(indexes, currentIndex + 1, values, valueLow);
+        flunctuate(indexes, currentIndex + 1, values, valueHigh);
+    }
+}
+
+fn doPart2(line: &String, memory:& mut HashMap<usize, u64>, orMask: u64, flunctuations: &mut Vec<usize>)
+{
+    if line.contains("mask")
+    {
+        if let Ok((mask)) = scan_fmt!(&line,
+                        "mask = {}", String)
+        {
+            flunctuations.clear();
+            let mut index: usize = 36;
+            for character in mask.chars()
+            {
+                match character
+                {
+                    'X' => {(*flunctuations).push(index - 1)},
+                    _ => {},
+                }
+                index -= 1;
+
+            }
+            // for v in flunctuations
+            // {
+            //     print!("{} ", v);
+            // }
+            // println!("");
+
+            //println!("{:#038b}", andMask);
+            //println!("{:#038b}", orMask);
+        }
+    }
+    else
+    {
+        if let Ok((index, input)) = scan_fmt!(&line,
+                        "mem[{}] = {}", usize, u64)
+        {
+            //println!("Read {} {}", index, input);
+            //println!("Actual {}", actual);
+            let mut values: Vec<usize> = Vec::new();
+            let vv = index | orMask as usize;
+            flunctuate(flunctuations, 0, &mut values, vv);
+            for v in values
+            {
+                memory.insert(v, input);
+            }
+
+        }
+    }
+}
+
 pub fn printResult()
 {
     if let Ok(lines) = fileUtil::readLines("data/input14.txt")
@@ -52,6 +117,8 @@ pub fn printResult()
         let mut andMask = 0;
         let mut orMask = 0;
         let mut memory: HashMap<usize, u64> = HashMap::new();
+        let mut memory2: HashMap<usize, u64> = HashMap::new();
+        let mut flunctuations: Vec<usize> = Vec::new();
         for line in lines
         {
             if let Ok(value) = line
@@ -60,6 +127,8 @@ pub fn printResult()
                 {
                     doPart1(&value, & mut memory, & mut andMask, & mut orMask);
 
+                    //Part2 relies on orMask from part 1.
+                    doPart2(&value, & mut memory2, orMask, &mut flunctuations);
                 }
             }
         }
@@ -68,7 +137,13 @@ pub fn printResult()
         {
             sum += value.1;
         }
+        println!("The sum is {}", sum);
 
+        sum = 0;
+        for value in memory2
+        {
+            sum += value.1;
+        }
         println!("The sum is {}", sum);
     }
 }
